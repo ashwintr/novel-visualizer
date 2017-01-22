83fa6d41cfab35b0ddc1e02c69adb301744f5b5d
Ashwin <ashwintr10@gmail.com>
2017-01-22 11:47:51 +0530

    Update routingscript.js
    making responsive as main page

--- NovelVisualiser/scripts/routingscript.js
+++ NovelVisualiser/scripts/routingscript.js
@@ -2,18 +2,18 @@
   var app = angular.module("myApp",["ngRoute"]);     /*global angular*/
     app.config(function($routeProvider) {
   	  $routeProvider
-  		.when("/responsive", {
-  			templateUrl: "/novel-visualizer/NovelVisualiser/templates/responsive.html",
-  			controller: "responsiveController"
-  		})
   		.when("/mainPage", {
   			templateUrl: "/novel-visualizer/NovelVisualiser/templates/mainpage.html",
   			controller: "mainController"
+  		})
+  		.when("/responsive", {
+  			templateUrl: "/novel-visualizer/NovelVisualiser/templates/responsive.html",
+  			controller: "responsiveController"
   		})
   		.when("/addNovels", {
   		  templateUrl: "/novel-visualizer/NovelVisualiser/templates/addNovels.html",
   		  controller: "addNovelsController"
   		})
-  		.otherwise({redirectTo: "/mainPage"});
+  		.otherwise({redirectTo: "/responsive"});
   });
 }());
