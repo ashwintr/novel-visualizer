fb20e7ce267b9cf0ae8a6f99abce78734fc48d80
Ashwin <ashwintr10@gmail.com>
2018-06-05 23:07:52 +0530

    Corrected the path for scripts

--- scripts/routingscript.js
+++ scripts/routingscript.js
@@ -3,22 +3,22 @@
     app.config(function($routeProvider) {
   	  $routeProvider
   		.when("/mainPage", {
-  			templateUrl: "/novel-visualizer/NovelVisualiser/templates/mainpage.html",
+  			templateUrl: "/templates/mainpage.html",
         /*templateUrl: "/NovelVisualiser/templates/mainpage.html",*/
   			controller: "mainController"
   		})
   		.when("/responsive", {
-  			templateUrl: "/novel-visualizer/NovelVisualiser/templates/responsive.html",
+  			templateUrl: "/templates/responsive.html",
         /*templateUrl: "/NovelVisualiser/templates/responsive.html",*/
   			controller: "responsiveController"
   		})
   		.when("/addNovels", {
-  		  templateUrl: "/novel-visualizer/NovelVisualiser/templates/addNovels.html",
+  		  templateUrl: "/templates/addNovels.html",
         /*templateUrl: "/NovelVisualiser/templates/addNovels.html",*/
   		  controller: "addNovelsController"
   		})
       .when("/novellist", {
-        templateUrl: "/novel-visualizer/NovelVisualiser/templates/novellist.html",
+        templateUrl: "/templates/novellist.html",
         /*templateUrl: "/NovelVisualiser/templates/novellist.html",*/
         controller: "novellistController"
       })
